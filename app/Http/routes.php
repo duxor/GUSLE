7cd24ee2a4a49fd96fac5617b8e169e10dd45e3f
AJovic <ajovic990@gmail.com>
2015-12-16 13:29:57 +0100

    Auth
    Auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,14 +14,19 @@
 // Authentication routes...
 use Illuminate\Support\Facades\Auth;
 
+// {{ A J }}}
+//Registrovenje
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+//Prijavljivanje
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
+//Resetovanje sifre
+Route::get('/password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
 
 
 Route::controllers([
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,48 @@
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+@include('layouts.head')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Пријављивање</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Хеј!</strong> Постоји неки проблем приликом уноса података.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Корисничко име</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="username" value="{{ old('username') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Шифра</label>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">Пријави се</button>
+
+                                <a class="btn btn-link" href="{{ url('/password/email') }}">Заборавили сте шифру?</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-@endif
-                {!!Form::open(['url'=>'/auth/login'])!!}
-                    {!! csrf_field() !!}
-                    {!!Form::text('username','dusan.perisic')!!}
-                    {!!Form::password('password',['value'=>'12345678'])!!}
-                    {!!Form::submit('Login')!!}
-                {!!Form::close()!!}
-                <p>Potrebno je ponovo uraditi migraciju i seed kako bi se prava pristupa podesila za sve korisnike.</p>
-                <button onclick="popuni('dusan.perisic','12345678')">Korisnik Pravo 1</button>
-                <button onclick="popuni('petar.petrovic','12345678')">Korisnik Pravo 2</button>
-                <button onclick="popuni('milovan.milosevic','12345678')">Korisnik Pravo 3</button>
-                <script>function popuni(u,p){document.getElementsByName('password')[0].value=p;document.getElementsByName('username')[0].value=u;}</script>
+</div>
